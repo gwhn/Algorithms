7a662637d127f53b3f81e6e3a22716b723b43d71
Guy Newman <gwhn@weblitz.co.uk>
2013-09-16 15:16:41 +0100

    Binary search for square root

---
+++ Algorithms.Code/Searching/SquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Code.Searching
+{
+    public static class SquareRoot
+    {
+        public static double Calculate(double n)
+        {
+            double low = 0.0D;
+            double high = Math.Max(1, n);
+            double mid;
+            while (low + 1e-3 < high)
+            {
+                mid = (low + high)/2.0D;
+                if (mid * mid < n)
+                {
+                    low = mid;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+            return low;
+        }
+    }
+}
---
+++ Algorithms.Tests/Searching/SquareRoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests.Searching
+{
+    [TestClass]
+    public class SquareRoot
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            const double n = 81.0D;
+            var result = Code.Searching.SquareRoot.Calculate(n);
+            Assert.IsTrue(result > 9.0D - 1e-3 && result < 9.0D + 1e-3);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            const double n = 2.618D;
+            var result = Code.Searching.SquareRoot.Calculate(n);
+            Assert.IsTrue(result > 1.618D - 1e-3 && result < 1.618D + 1e-3);
+        }
+    }
+}
