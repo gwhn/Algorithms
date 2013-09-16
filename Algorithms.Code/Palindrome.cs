c474a3a528eff8c1a4852a09caa5718357d4cb3d
Guy Newman <gwhn@weblitz.co.uk>
2013-09-16 13:55:36 +0100

    Palindrome tests for strings and numbers

---
+++ Algorithms.Code/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Code
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrome(int number)
+        {
+            var reverse = 0;
+            var temp = number;
+            while (temp > 0)
+            {
+                reverse = reverse*10 + temp%10;
+                temp /= 10;
+            }
+            return number == reverse;
+        }
+
+        public static bool IsPalindrome(string text)
+        {
+            int n = text.Length;
+            var sb = new StringBuilder(n);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                sb.Append(text[i]);
+            }
+            return text == sb.ToString();
+        }
+    }
+}
---
+++ Algorithms.Tests/Palindrome.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class Palindrome
+    {
+        [TestMethod]
+        public void NumberIsPalindrome()
+        {
+            const int number = 1234321;
+            var result = Code.Palindrome.IsPalindrome(number);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NumberIsNotPalindrome()
+        {
+            const int number = 12345321;
+            var result = Code.Palindrome.IsPalindrome(number);
+            Assert.IsFalse(result);            
+        }
+
+        [TestMethod]
+        public void StringIsPalindrome()
+        {
+            const string text = "tatarratat";
+            var result = Code.Palindrome.IsPalindrome(text);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void StringIsNotPalindrome()
+        {
+            const string text = "something";
+            var result = Code.Palindrome.IsPalindrome(text);
+            Assert.IsFalse(result);            
+        }
+    }
+}
